47990111fcf77cfcbe26dbef53994f481f981e88
Yaroslav Novak <yaroslav.novak@fortegrp.net>
2018-06-23 09:32:33 -0500

    changed formatting

--- CurrentYearWeekdaysStartingFromToday.py
+++ CurrentYearWeekdaysStartingFromToday.py
@@ -5,10 +5,12 @@ import datetime
 today = datetime.date.today()
 last_day = today.replace(month=12, day=31)
 
+
 def is_weekday(day):
-  return day.weekday() < 5
+    return day.weekday() < 5
+
 
 nextday = today
 while nextday <= last_day:
-	print (nextday.strftime("%m/%d/%Y") if is_weekday(nextday) else '')
-	nextday += datetime.timedelta(days=1)
+    print(nextday.strftime("%m/%d/%Y") if is_weekday(nextday) else '')
+    nextday += datetime.timedelta(days=1)
