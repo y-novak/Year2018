962e4d2d85fa37c1cb13daa05df2bfca926e010c
Yaroslav Novak <yaroslav.novak@fortegrp.net>
2018-06-23 09:21:22 -0500

    Create CurrentYearWeekdaysStartingFromToday.py

---
+++ CurrentYearWeekdaysStartingFromToday.py
@@ -0,0 +1,12 @@
+import datetime
+
+today = datetime.date.today()
+last_day = today.replace(month=12, day=31)
+
+def is_weekday(day):
+  return day.weekday() < 5
+
+nextday = today
+while nextday <= last_day:
+	print (nextday.strftime("%m/%d/%Y") if is_weekday(nextday) else '')
+	nextday += datetime.timedelta(days=1)
