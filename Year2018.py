a19ad4763dcdb341509aa79639e2514c122e301b
Yaroslav Novak <yaroslav.novak@fortegrp.net>
2020-02-10 06:14:53 -0600

    Converted Python 2 to Python 3

--- Year2018.py
+++
@@ -1,5 +0,0 @@
-from calendar import monthrange
-y = 2018
-for m in range(1, 13):
-    for d in range(1, monthrange(y ,m)[1] + 1):
-        print '%s%s/%s%s/%s'% ('0' if m < 10 else '', m,'0' if d < 10 else '', d, y)
